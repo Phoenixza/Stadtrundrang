7ee52211c039701731283a715bdcf8a288b3f143
Phoenixza <m.welser@gmx.net>
2015-01-08 20:33:13 +0100

    Stadtrundgang App für Android WPV

---
+++ Stadtrundgang/gen/com/example/stadtrundgang/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.stadtrundgang;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Stadtrundgang/src/com/example/stadtrundgang/GoogleMapsActivity.java
@@ -0,0 +1,5 @@
+package com.example.stadtrundgang;
+
+public class GoogleMapsActivity {
+
+}
