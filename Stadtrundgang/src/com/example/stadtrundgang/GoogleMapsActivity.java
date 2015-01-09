93c3a772cf860c6cb63f3ca9a5d6f0a21d6e0768
Phoenixza <m.welser@gmx.net>
2015-01-09 07:43:40 +0100

    Update: GoogleMaps runs now

--- Stadtrundgang/src/com/example/stadtrundgang/GoogleMapsActivity.java
+++ Stadtrundgang/src/com/example/stadtrundgang/GoogleMapsActivity.java
@@ -1,5 +1,13 @@
 package com.example.stadtrundgang;
 
-public class GoogleMapsActivity {
+import android.app.Activity;
+import android.os.Bundle;
 
+public class GoogleMapsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.google_maps);
+    }
 }
--- Stadtrundgang/src/com/example/stadtrundgang/MainActivity.java
+++ Stadtrundgang/src/com/example/stadtrundgang/MainActivity.java
@@ -1,28 +1,38 @@
 package com.example.stadtrundgang;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	Button btn_maps;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		findViewById(R.id.btn_gmap).setOnClickListener( new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+		        //Inform the user the button has been clicked
+		        Toast.makeText(MainActivity.this, "Button1 clicked.", Toast.LENGTH_SHORT).show(); 
+		        Intent intent = new Intent(MainActivity.this, GoogleMapsActivity.class);
+	            startActivity(intent);
+		    }
+		});
 	}
 
-	
-	public void onClick(View v){
-		
-		
-		
-		
-		
-	}
-	
-	
 
 }
+	
+	
+
+
